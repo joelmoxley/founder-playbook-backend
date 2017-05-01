6c7d58e276456dd3f6fa686b8b38bb971cb9fb73
rchipka <rchipka@vibethink.com>
2017-05-01 17:09:11 -0400

    Update clone script

--- clone.php
+++ clone.php
@@ -1,12 +1,12 @@
 <?php
-$gitURL = 'https://github.com/joelmoxley/founder-playbook/tree/develop';
+$gitURL = 'https://github.com/joelmoxley/founder-playbook.git';
 
-if (file_exists('content') && !file_exists('content/.git')) {
+if (file_exists('content')) {
   exec('rm -rf content');
 }
 
 if (file_exists('content')) {
-  exec('cd content && git reset --hard HEAD && git pull > /dev/null 2>/dev/null &');
+  exec('cd content && git reset --hard HEAD && git pull > /dev/null 2>/dev/null && git checkout develop');
 } else {
   exec('git clone ' . $gitURL . ' content > /dev/null 2>/dev/null &');
 }
